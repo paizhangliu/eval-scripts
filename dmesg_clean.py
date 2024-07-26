af9e2f672ffc2fd10f1a80a830a4d5ec22ec320d
paizhang <paizhangpi@outlook.com>
2024-07-26 19:28:37 +0800

    bugfix

--- dmesg_clean.py
+++ dmesg_clean.py
@@ -9,24 +9,13 @@ import os
 
 # Print usage and exit abnormally
 def print_usage_and_exit(args):
-    print("Usage:", args[0], "[input file]", "[-o output file]", "[-sb keep square backets]", "[-b keep brackets]")
+    print("Usage:", args[0], "[input file]", "[-o output file]", "[-sb keep square backets]", "[-b keep brackets]", "[-q quiet]")
+    print("Cannot keep both (-sb and -b). Must have an output (not -q or -o).")
     sys.exit(-1)
 
 # Parse/get arguments
 args = sys.argv.copy()
 if len(args) < 2:
-    print_usage_and_exit(args)
-
-out = None
-if "-o" in args:
-    try:
-        out = args.pop(args.index("-o") + 1)
-        args.remove("-o")
-    except:
-        print("Invalid output file")
-        print_usage_and_exit(args)
-else:
-    print("Please specify an output file")
     print_usage_and_exit(args)
 
 sb = False
@@ -40,8 +29,24 @@ if "-b" in args:
     args.remove("-b")
 
 if sb and b:
-    print("Keeping everything does not make any sense, exiting.")
-    exit(-1)
+    print_usage_and_exit(args)
+
+out = None
+if "-o" in args:
+    try:
+        out = args.pop(args.index("-o") + 1)
+        args.remove("-o")
+    except:
+        print("Invalid output file")
+        print_usage_and_exit(args)
+
+q = False
+if "-q" in args:
+    q = True
+    args.remove("-q")
+
+if not out and q:
+    print_usage_and_exit(args)
 
 try:
     arg = args.pop(1)
@@ -69,38 +74,38 @@ except:
     print_usage_and_exit(args)
 
 # Purge square brackets
-i = 0
 if not sb:
     for l in range(0, len(file)):
         if file[l][0] != '[':
             continue
-        for c in file[l]:
-            if c == ']':
+        for i in range(1, len(file)):
+            if file[l][i] == ']':
                 break
-            i += 1
         file[l] = file[l][i + 2:]
 
 # Purge brackets
 if not b:
     for l in range(0, len(file)):
-        i = len(file[l]) - 1
-        if file[l][i] != ')':
+        if len(file[l]) < 2 or file[l][len(file[l]) - 2] != ')':
             continue
-        for c in reversed(file[l]):
-            if c == '(':
+        for i in range(len(file[l]) - 3, -1, -1):
+            if file[l][i] == '(':
                 break
-            i -= 1
         if file[l][i:i + 4] != "(at ":
             continue
-        file[l] = file[l][:i - 1]
+        file[l] = file[l][:i - 1] + '\n'
+
+# Print to the terminal
+if not q:
+    for l in file:
+        print(l, end="")
 
 # Write output file
-try:
-    with open(out, "w") as outfile:
-        for i in range(0, len(file)):
-            outfile.write(file[i])
-except:
-    print("Error writing file:", out)
-    exit(-1)
-
-print("Done.")
+if out:
+    try:
+        with open(out, "w") as outfile:
+            for i in range(0, len(file)):
+                outfile.write(file[i])
+    except:
+        print("Error writing file:", out)
+        exit(-1)
